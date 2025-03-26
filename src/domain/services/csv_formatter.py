c10113621c138e54043b72b133287d646dd07437
ccat <ccathlete01@gmail.com>
2025-03-26 10:32:06 +0100

    Fixed an import in main module.

--- src/domain/services/csv_formatter.py
+++ src/domain/services/csv_formatter.py
@@ -29,13 +29,3 @@ class CsvFormatter:
                 valid_lines.append(",".join(processed_values).rstrip("_"))
 
         return valid_lines
-
-
-# Read from stdin and process the data
-if __name__ == "__main__":
-    # input_data: str = sys.stdin.readlines()
-    # print(repr(input_data))
-    output = CsvFormatter(
-        input_stream=sys.stdin.readlines(),
-    ).process_csv()
-    print("\n".join(output), end="")  # Print to stdout (NiFi will capture this)
