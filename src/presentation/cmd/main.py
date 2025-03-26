7370b088f0f99beb0d6f0ffd3c165f9b14ef0a1d
ccat <ccathlete01@gmail.com>
2025-03-26 10:29:50 +0100

    Added domain service logic for formatting csv.

--- src/presentation/cmd/main.py
+++ src/presentation/cmd/main.py
@@ -2,10 +2,14 @@ from src.domain.services.csv_formatter import CsvFormatter
 import sys
 
 
-if __name__ == "__main__":
+def input_from_nifi() -> None:
     # input_data: str = sys.stdin.readlines()
     # print(repr(input_data))
-    output = CsvFormatter(
+    output: list[str] = CsvFormatter(
         input_stream=sys.stdin.readlines(),
     ).process_csv()
     print("\n".join(output), end="")  # Print to stdout (NiFi will capture this)
+
+
+if __name__ == "__main__":
+    input_from_nifi()
